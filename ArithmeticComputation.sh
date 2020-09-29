f1edc96ca5d4f74f9e4a7247247a74ab840cb950
Suraj <surajjadhav9741@gmail.com>
2020-09-29 22:56:44 +0530

    Compute=a*b+c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -4,5 +4,7 @@ echo "Enter three value for input : "
 read a
 read b
 read c
-compute=$((a*b+c))
-echo "a*b+c=$compute"
+compute=$((a+b*c))
+compute1=$((a*b+c))
+echo "a+b*c=$compute"
+echo "a*b+c=$compute1"
