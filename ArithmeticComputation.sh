936a6e6a747a0ab882b99be95599459d29dc9a07
Suraj <surajjadhav9741@gmail.com>
2020-09-30 00:14:36 +0530

    Add : Read the values from the Dictionary into the array

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -9,7 +9,12 @@ compute[0]=$(awk 'BEGIN{print '$a' + '$b' * '$c'}')
 compute[1]=$(awk 'BEGIN{print '$a' * '$b' + '$c'}')
 compute[2]=$(awk 'BEGIN{print '$c' + '$a' / '$b'}')
 compute[3]=$(awk 'BEGIN{print '$a' % '$b' / '$c'}')
+for ((i=0;i<${#compute[@]};i++))
+do
+	d[$i]=$compute[$i]
+done
 echo "a + b * c = ${compute[0]}"
 echo "a * b + c = ${compute[1]}"
 echo "c + a / b = ${compute[2]}"
 echo "a % b / c = ${compute[3]}"
+echo "${compute[@]}"
