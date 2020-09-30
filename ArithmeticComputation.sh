c348b2d29cb6df839f963e66670462fa140841c0
Suraj <surajjadhav9741@gmail.com>
2020-09-30 09:48:44 +0530

    Add : Sort the results to show the Computation Result in the Descending Order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,9 +1,7 @@
 ##!/bin/bash -x
 echo "Arithmetic Computation & Sorting"
 echo "Enter three value for input : "
-read a
-read b
-read c
+read a b c
 declare -A compute
 compute[0]=$(awk 'BEGIN{print '$a' + '$b' * '$c'}')
 compute[1]=$(awk 'BEGIN{print '$a' * '$b' + '$c'}')
@@ -11,10 +9,15 @@ compute[2]=$(awk 'BEGIN{print '$c' + '$a' / '$b'}')
 compute[3]=$(awk 'BEGIN{print '$a' % '$b' / '$c'}')
 for ((i=0;i<${#compute[@]};i++))
 do
-	d[$i]=$compute[$i]
-done | sort -n
+	d[$i]=${compute[$i]}
+done
 echo "a + b * c = ${compute[0]}"
 echo "a * b + c = ${compute[1]}"
 echo "c + a / b = ${compute[2]}"
 echo "a % b / c = ${compute[3]}"
-echo "Sort According Descending Order : ${compute[@]}"
+echo "Array stored values : ${d[@]}"
+echo "Sort According Descending Order :"
+for ((i=0;i<=${#d[@]};i++))
+do
+        echo "${d[$i]}"
+done | sort -nr
