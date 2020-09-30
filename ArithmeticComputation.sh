ccb0f8aaf0bc8cc917eb4440104b8d5d24a5f078
Suraj <surajjadhav9741@gmail.com>
2020-09-30 09:32:04 +0530

    Add : Sort the results to show the Computation Value in Ascend

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,20 +1,29 @@
 ##!/bin/bash -x
 echo "Arithmetic Computation & Sorting"
 echo "Enter three value for input : "
-read a
-read b
-read c
+read a b c
+arr=()
 declare -A compute
 compute[0]=$(awk 'BEGIN{print '$a' + '$b' * '$c'}')
 compute[1]=$(awk 'BEGIN{print '$a' * '$b' + '$c'}')
 compute[2]=$(awk 'BEGIN{print '$c' + '$a' / '$b'}')
 compute[3]=$(awk 'BEGIN{print '$a' % '$b' / '$c'}')
-for ((i=0;i<${#compute[@]};i++))
-do
-	d[$i]=$compute[$i]
-done | sort -n
 echo "a + b * c = ${compute[0]}"
 echo "a * b + c = ${compute[1]}"
 echo "c + a / b = ${compute[2]}"
 echo "a % b / c = ${compute[3]}"
-echo "Sort According Descending Order : ${compute[@]}"
+for ((i=0;i<=${#compute[@]};i++))
+do
+	d[$i]=${compute[$i]}
+done
+echo "Array stored values : ${d[@]}"
+echo "Sort According Descending Order :"
+for ((i=0;i<=${#d[@]};i++))
+do
+        echo "${d[$i]}"
+done | sort -nr
+echo "Sort According Ascending Order :"
+for ((i=0;i<=${#d[@]};i++))
+do
+        echo "${d[$i]}"
+done | sort -n
